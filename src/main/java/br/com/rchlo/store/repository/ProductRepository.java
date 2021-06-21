596ec1228952ec65d2dab523e7c88d5b4d1bdead
samuel.guimaraes <samuel.guimaraes@cabal.com.br>
2021-06-21 13:13:22 -0300

    Corrige implementação da consulta realizada no método findAllByOrderByName

--- src/main/java/br/com/rchlo/store/repository/ProductRepository.java
+++ src/main/java/br/com/rchlo/store/repository/ProductRepository.java
@@ -9,19 +9,21 @@ import java.util.List;
 
 @Repository
 public class ProductRepository {
-
-    private final EntityManager entityManager;
-
-    public ProductRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    public List<Product> findAllByOrderByName() {
-        return entityManager.createQuery("select p from Product p", Product.class).getResultList();
-    }
-
-    public List<ProductByColorDto> productsByColor() {
-        return entityManager.createQuery("select new br.com.rchlo.store.dto.ProductByColorDto(p.color, count(p)) from Product p group by p.color", ProductByColorDto.class).getResultList();
-    }
-
+	
+	private final EntityManager entityManager;
+	
+	public ProductRepository(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	public List<Product> findAllByOrderByName() {
+		return this.entityManager.createQuery("select p from Product p ORDER BY p.name", Product.class).getResultList();
+	}
+	
+	public List<ProductByColorDto> productsByColor() {
+		return this.entityManager.createQuery(
+				"select new br.com.rchlo.store.dto.ProductByColorDto(p.color, count(p)) from Product p group by p.color",
+				ProductByColorDto.class).getResultList();
+	}
+	
 }
